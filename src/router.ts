645a5587802b798b3ecde837c40e228ee2a649bf
Filip Kin <filip@kinmails.com>
2019-03-09 12:46:43 -0500

    Updated logger

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createServer } from 'http';
 import { createProxyServer } from 'http-proxy';
 import { timer } from './util/timer';
+import { Logger } from './util/logger';
+let log = new Logger('Router', 'magenta');
 
 let c = {config:{ports:{proxy:0,sslProxy:0,frontend:0,docs:0,mysql:0,api:0,}}}
 
-export async function startProxy(database, type) {
+export async function startRouter(database, type='http') {
 
     let port = (type === 'http') ? c.config.ports.proxy : c.config.ports.sslProxy;
 
@@ -25,44 +27,44 @@ export async function startProxy(database, type) {
         let domain = req.headers.host;
         if (domain.startsWith('www.')) domain = domain.substring(4);
         let ip = req.connection.remoteAddress;
-        console.log('\n' + ip.replace('::ffff:', '') + ' --> ' + domain);
+        log.log('\n' + ip.replace('::ffff:', '') + ' --> ' + domain);
 
         // Handle internal requests
         if (domain === 'docs.kiwahosting.com') {
             proxy.web(req, res, {target: type+'://127.0.0.1:'+c.config.ports.docs});
-            console.log('`-----> ' + 'docs' + '|');
+            log.log('`-----> ' + 'docs' + '|');
             timer(start, 'Proxy took');
         } else if ( domain === 'api.kiwahosting.com') {
             proxy.web(req, res, {target: type+'://127.0.0.1:'+c.config.ports.api});
-            console.log('`-----> ' + 'API' + '|');
+            log.log('`-----> ' + 'API' + '|');
             timer(start, 'Proxy took');
         } else if ( domain === 'mysql.kiwahosting.com') {
             proxy.web(req, res, {target: type+'://127.0.0.1:'+c.config.ports.mysql});
-            console.log('`-----> ' + 'mysql' + '|');
+            log.log('`-----> ' + 'mysql' + '|');
             timer(start, 'Proxy took');
         } else if ( domain === 'kiwahosting.com' || domain === 'www.kiwahosting.com') {
             proxy.web(req, res, {target: type+'://127.0.0.1:'+c.config.ports.frontend});
-            console.log('`-----> ' + 'frontend' + '|');
+            log.log('`-----> ' + 'frontend' + '|');
             timer(start, 'Proxy took');
         } else {
             // Handle client requests
             let sql = "SELECT * FROM `domains` WHERE `name` = '" + domain + "'";
             database.query(sql).then(function(rows) {
                 if (rows == undefined) {
-                    console.log('rows undefined');
+                    log.log('rows undefined');
                     res.write('<html><body><h2>Domain not connected to site</h2>');
                     res.write('<p>If you own this domain, make sure its added in the dashboard</p></body></html>');
                     res.end();
                 } else {
                     let row = rows[0];
                     if (row == undefined) {
-                        console.log('disconnected site');
+                        log.log('disconnected site');
                         res.write('<html><body><h2>Domain not connected to site</h2>');
                         res.write('<p>If you own this domain, make sure its added in the dashboard</p></body></html>');
                         res.end();
                     } else {
                         let path = type+'://' + row.server + ':' + row.port + row.path;
-                        console.log('`-----> ' + row.siteid + ' - ' + path + ' |');
+                        log.log('`-----> ' + row.siteid + ' - ' + path + ' |');
                         proxy.web(req, res, {target: path});
                     }
                 }
@@ -75,5 +77,5 @@ export async function startProxy(database, type) {
         }
     }).listen(port);
 
-    console.info('Proxy server started on '+type+'://127.0.0.1:' + port);
+    log.log('Proxy server started on '+type+'://127.0.0.1:' + port);
 }
